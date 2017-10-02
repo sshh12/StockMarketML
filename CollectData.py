cadcb2a95c7149f9b793ebc101cc1682272ee198
Shrivu Shankar <shrivu1122@gmail.com>
2017-10-01 21:21:13 -0500

    Update

--- CollectData.py
+++ CollectData.py
@@ -5,21 +5,53 @@
 
 # Setup (Imports)
 
-from collections import defaultdict
 
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+
+from collections import defaultdict
 from datetime import datetime
 from praw import Reddit
+
 import requests
+import os
 
 
 # In[2]:
+
+
+def process_raw_text(text):
+
+    tokenizer = RegexpTokenizer(r'\w+')
+    text_processed = tokenizer.tokenize(text)
+    
+    text_processed = [word.lower() for word in text_processed if word.lower() not in stopwords.words('english')]
+
+    porter_stemmer = PorterStemmer()
+
+    text_processed = [porter_stemmer.stem(word) for word in text_processed]
+
+    return " ".join(text_processed)
+
+
+# In[3]:
+
 
 reddit = Reddit('StockMarketML')
 
 articles = defaultdict(list)
 
 for submission in reddit.subreddit('news+apple+ios+AAPL').search('apple', limit=None):
+    
     articles[datetime.fromtimestamp(submission.created).strftime('%Y-%m-%d')].append(submission.title)
     
-articles
+with open(os.path.join('data', 'reddit.csv'), 'w') as redditfile:
+    
+    for date, sents in articles.items():
+        
+        data = str(sents).encode("utf-8")
+    
+        redditfile.write(date + ", " + str(data)[1:] + "\n")
+    
 

a10a38d81a8a4f1064ff93c6d65b5b986ee2365b
Shrivu Shankar <shrivu1122@gmail.com>
2017-10-29 15:59:50 -0500

    Tweaks

--- HeadlinePredictionClassification.py
+++ HeadlinePredictionClassification.py
@@ -10,7 +10,7 @@ from LoadData import *
 from keras.models import Sequential
 from keras.layers.advanced_activations import LeakyReLU
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
-from keras.layers import Dense, LSTM, Dropout, Flatten, Conv1D, BatchNormalization, Activation
+from keras.layers import Dense, LSTM, Dropout, Flatten, Conv1D, BatchNormalization, Activation, GlobalMaxPooling1D
 
 import numpy as np
 
@@ -21,9 +21,9 @@ import matplotlib.pyplot as plt
 
 # Setup (Globals/Hyperz)
 
-window_size  = 5
-epochs       = 500
-batch_size   = 64
+window_size  = 6
+epochs       = 750
+batch_size   = 128
 emb_size     = 100
 
 
@@ -33,9 +33,9 @@ emb_size     = 100
 
 def get_data(stock):
     
-    AllX, AllY = create_timeframed_doc2vec_classification_data(stock, window_size)
+    AllX, AllY = create_timeframed_doc2vec_classification_data(stock, window_size, min_time_disparity=4)
     
-    trainX, trainY, testX, testY = split_data(AllX, AllY, ratio=.9)
+    trainX, trainY, testX, testY = split_data(AllX, AllY, ratio=.85)
     
     return (trainX, trainY), (testX, testY)
 
@@ -46,26 +46,27 @@ def get_model():
     
     model = Sequential()
     
-    #model.add(Conv1D(filters=16, kernel_size=3, padding='same', input_shape=(window_size, emb_size)))
-    #model.add(BatchNormalization())
-    model.add(LSTM(80, input_shape=(window_size, emb_size)))
-    model.add(Activation('relu'))
-    #model.add(Flatten())
-    model.add(Dropout(0.3))
+    model.add(Conv1D(filters=32, kernel_size=3, padding='valid', input_shape=(window_size, emb_size)))
+    model.add(GlobalMaxPooling1D())
+    
+    model.add(Dense(30))
+    model.add(BatchNormalization())
+    model.add(Activation('selu'))
+    model.add(Dropout(0.5))
+    
+    model.add(Dense(20))
+    model.add(BatchNormalization())
+    model.add(Activation('selu'))
+    model.add(Dropout(0.5))
     
     model.add(Dense(10))
     model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.3))
-    
-    model.add(Dense(10))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.3))
+    model.add(Activation('selu'))
+    model.add(Dropout(0.5))
 
     model.add(Dense(2, activation='softmax'))
     
-    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         
     return model
 
@@ -81,7 +82,7 @@ if __name__ == "__main__":
     print(trainX.shape, trainY.shape)
 
 
-# In[8]:
+# In[6]:
 
 # Run (Train)
 
@@ -90,9 +91,9 @@ if __name__ == "__main__":
     model = get_model()
 
     reduce_LR = ReduceLROnPlateau(monitor='val_acc', factor=0.9, patience=30, min_lr=1e-6, verbose=0)
-    e_stopping = EarlyStopping(patience=30)
+    e_stopping = EarlyStopping(patience=60)
     checkpoint = ModelCheckpoint(os.path.join('models', 'headline-classification.h5'), 
-                                 monitor='val_acc', 
+                                 monitor='val_loss', 
                                  verbose=0, 
                                  save_best_only=True)
 
@@ -100,7 +101,7 @@ if __name__ == "__main__":
                                         batch_size=batch_size, 
                                         validation_data=(testX, testY), 
                                         verbose=0, 
-                                        callbacks=[e_stopping, checkpoint])
+                                        callbacks=[checkpoint, e_stopping])
 
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])

570745214f06e959814c982384f3fcb9da0436dd
Shrivu Shankar <shrivu1122@gmail.com>
2017-12-01 16:32:22 -0600

    Tweak

--- Variation2/TrendPrediction.py
+++ Variation2/TrendPrediction.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 
 # Keras
 
+import keras.backend as K
 from keras import regularizers
 from keras import optimizers
 from keras.models import Sequential
@@ -32,20 +33,30 @@ from keras.layers import Dense, LSTM, Dropout, Flatten, Conv1D, BatchNormalizati
 epochs           = 500
 batch_size       = 64
 
-window_size      = 30
+window_size      = 60
 skip_window_size = 5
 
 train_split      = .9
 emb_size         = 5
 
 
-# In[4]:
+# In[16]:
 
 # Load Data
 
 def create_timeframed_alldata_data(stocks, window_size=10, skip_window_size=2):
     """
     Timeframe Alldata
+    
+    Parameters
+    ----------
+    stocks : list `str`
+        The names of the stocks to use (pulled by name from data dir)
+    
+    Returns
+    -------
+    X, Y : tuple `np.array`
+        The X and Y data
     """
     X, Y = [], []
     
@@ -83,21 +94,16 @@ def create_timeframed_alldata_data(stocks, window_size=10, skip_window_size=2):
             time_frame /= (np.std(trainable_frame, axis=0) + 1e-7)
 
             X.append(trainable_frame)
-
+            
             if last_close < target_close:
-
-                Y.append([1.])
-
+                Y.append([1., target_close - last_close])
             else:
-
-                Y.append([0.])
-                
-            Y[-1].append(time_frame[-1, 3])
-        
+                Y.append([0., target_close - last_close])
+            
     return np.array(X), np.array(Y)
 
 
-# In[5]:
+# In[17]:
 
 # Split
 
@@ -120,25 +126,31 @@ def get_data(stocks):
     
     X, Y = create_timeframed_alldata_data(stocks, window_size=window_size, skip_window_size=skip_window_size)
     
+    Y[:, 0] /= np.std(Y[:, 0])
+    
     return split_data(X, Y, ratio=train_split)
 
 
-# In[6]:
+# In[18]:
 
 # Model
+
+def binacc(y_true, y_pred):
+    """
+    Binary Accuracy
+    
+    Keras metric to compute the %accuracy given predicted vs actual
+    """
+    return K.mean(K.equal(y_true[:, 0] >= 0, y_pred[:, 0] >= 0), axis=-1)
 
 def get_model():
     
     model = Sequential()
 
     model.add(LSTM(200, input_shape=(window_size - skip_window_size, emb_size)))
-    
-    model.add(Dense(200))
-    model.add(BatchNormalization())
-    model.add(LeakyReLU())
     model.add(Dropout(0.3))
     
-    model.add(Dense(100))
+    model.add(Dense(200))
     model.add(BatchNormalization())
     model.add(LeakyReLU())
     model.add(Dropout(0.3))
@@ -155,18 +167,18 @@ def get_model():
 
     model.add(Dense(2))
     
-    model.compile(loss='mse', optimizer='adam')
+    model.compile(loss='mse', optimizer='adam', metrics=[binacc])
         
     return model
 
 
-# In[ ]:
+# In[19]:
 
 # Load Data
 
 if __name__ == "__main__":
     
-    trainX, trainY, testX, testY = get_data(['AAPL', 'GOOG', 'MSFT'])
+    trainX, trainY, testX, testY = get_data(['AAPL', 'GOOG'])
     
     print(trainX.shape, trainY.shape)
 
@@ -180,25 +192,30 @@ if __name__ == "__main__":
     model = get_model()
 
     reduce_LR = ReduceLROnPlateau(monitor='val_acc', factor=0.9, patience=30, min_lr=1e-6, verbose=0)
-    e_stopping = EarlyStopping(patience=50)
+    e_stopping = EarlyStopping(monitor='val_binacc', patience=50)
     checkpoint = ModelCheckpoint(os.path.join('..', 'models', 'trend-pred.h5'), 
-                                 monitor='val_loss', 
-                                 verbose=0, 
+                                 monitor='val_binacc', 
+                                 verbose=0,
                                  save_best_only=True)
 
     history = model.fit(trainX, trainY, epochs=epochs, 
                                         batch_size=batch_size, 
                                         validation_data=(testX, testY), 
                                         verbose=0, 
-                                        callbacks=[e_stopping])
+                                        callbacks=[e_stopping, checkpoint])
 
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
     plt.legend(['TrainLoss', 'TestLoss'])
     plt.show()
 
-    # plt.plot(history.history['acc'])
-    # plt.plot(history.history['val_acc'])
-    # plt.legend(['TrainAcc', 'TestAcc'])
-    # plt.show()
+    plt.plot(history.history['binacc'])
+    plt.plot(history.history['val_binacc'])
+    plt.legend(['TrainAcc', 'TestAcc'])
+    plt.show()
+
+
+# In[ ]:
+
+
 

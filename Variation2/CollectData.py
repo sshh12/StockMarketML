a6d49c41fb4ed2d4cd46059a501a04e0176a37c4
Shrivu Shankar <shrivu1122@gmail.com>
2017-12-18 13:27:59 -0600

    Tweaked Data Collection

--- Variation2/CollectData.py
+++ Variation2/CollectData.py
@@ -20,6 +20,14 @@ import re
 # In[2]:
 
 
+def strip_headline(headline):
+    
+    return headline.replace(',', '')
+
+
+# In[8]:
+
+
 def get_reddit_news(subs, search_terms, limit=None, praw_config='StockMarketML'):
     
     from praw import Reddit
@@ -37,8 +45,10 @@ def get_reddit_news(subs, search_terms, limit=None, praw_config='StockMarketML')
             if submission.title not in used:
                 
                 used.append(submission.title)
+                
+                date_key = datetime.fromtimestamp(submission.created).strftime('%Y-%m-%d')
 
-                articles[datetime.fromtimestamp(submission.created).strftime('%Y-%m-%d')].append(submission.title)
+                articles[date_key].append(submission.title)
         
     return articles
 
@@ -60,7 +70,11 @@ def get_reuters_news(stock, limit=400):
             
             headline = re.sub('[A-Z][A-Z\d\s]{5,}\-', '', headline)
             
-            articles[date_current.strftime('%Y-%m-%d')].append(headline)
+            date_key = date_current.strftime('%Y-%m-%d')
+            
+            if headline not in articles[date_key]:
+            
+                articles[date_key].append(headline)
         
             limit -= 1
         
@@ -68,7 +82,7 @@ def get_reuters_news(stock, limit=400):
         
     return articles
 
-def save_headlines(stock, sources, force_one_per_day=False):
+def save_headlines(stocks, sources, force_one_per_day=False):
     
     articles = defaultdict(list)
     
@@ -78,7 +92,7 @@ def save_headlines(stock, sources, force_one_per_day=False):
             
             articles[date].extend(source[date])
             
-    with open(os.path.join('..', 'data', stock + '-headlines.csv'), 'w', encoding="utf-8") as headline_file:
+    with open(os.path.join('..', 'data', "_".join(stocks) + '-headlines.csv'), 'w', encoding="utf-8") as headline_file:
         
         for date in sorted(articles):
             
@@ -87,96 +101,27 @@ def save_headlines(stock, sources, force_one_per_day=False):
             if force_one_per_day:
                 
                 current_articles = [random.choice(current_articles)]
+                
+            for headline in current_articles:
         
-            headline_file.write("{},{}\n".format(date, "@@".join(current_articles).replace(',', '')))
-
-
-# In[3]:
-
-
-if __name__ == "__main__":
-
-    save_headlines('AAPL', 
-                   [get_reddit_news(['apple', 'ios', 'AAPL', 'news'], ['apple', 'iphone', 'ipad', 'ios']), 
-                    get_reuters_news('AAPL.O')], 
-                    force_one_per_day=True)
+                headline_file.write("{},{}\n".format(date, strip_headline(headline)))
 
 
 # In[4]:
 
 
-def process_raw_text(text):
-
-    words = re.findall(r'\b\w+\b', text)
+if __name__ == "__main__":
     
-    cleaned = list(map(lambda w: w.lower(), words))
-
-    return " ".join(cleaned)
-
-def convert_headlines_to_vectors(stock, create_model=True):
-    
-    def read_headline_file():
-        
-        with open(os.path.join('..','data', stock + '-headlines.csv'), 'r', encoding="utf-8") as headline_file:
-        
-            for line in headline_file:
-            
-                if len(line) > 6:
-                
-                    date, headlines = line.split(',')
-                
-                    yield date, map(lambda x: x.strip(), headlines.split("@@"))
-    
-    if create_model:
-        
-        i = 0
-        
-        headlines_corpus = []
-        
-        for date, headlines in read_headline_file():
-            
-            for headline in headlines:
-                
-                if headline not in headlines_corpus:
-                
-                    headlines_corpus.append(LabeledSentence(process_raw_text(headline), tags=['headline_' + str(i)]))
-                
-                    i += 1
-        
-        doc_model = Doc2Vec(headlines_corpus, size=100, window=5, min_count=3, workers=4)
-        doc_model.save(os.path.join('..', 'models', stock + '-headlines-doc2vec.model'))
-    
-    doc_model = Doc2Vec.load(os.path.join('..', 'models', stock + '-headlines-doc2vec.model'))
-    
-    with open(os.path.join('..', 'data', stock + '-headlines-vectors.csv'), 'w', encoding="utf-8") as headline_vectors:
-        
-        i = 0
-        
-        used = []
-        
-        for date, headlines in read_headline_file(): #TODO file read not needed
-        
-            for headline in headlines:
-                
-                if headline not in used:
-                    
-                    used.append(headline)
-                
-                    vector = doc_model.docvecs[i]
-                
-                    vector = str(list(vector))
-                
-                    headline_vectors.write("{},{}\n".format(date, vector))
-                
-                i += 1
-    
-    return doc_model
+    headlines = [
+        get_reddit_news(['apple', 'ios', 'AAPL', 'news'], ['apple', 'iphone', 'ipad', 'ios']), 
+        get_reuters_news('AAPL.O', limit=10)
+    ]
 
 
-# In[5]:
+# In[9]:
 
 
 if __name__ == "__main__":
 
-    convert_headlines_to_vectors('AAPL')
+    save_headlines(['AAPL'], headlines, force_one_per_day=True)
 

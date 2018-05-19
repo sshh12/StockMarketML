54e960f5b66d5668a3d8ad810c24b43a3ea1c147
Shrivu Shankar <shrivu1122@gmail.com>
2018-05-19 15:56:00 -0500

    Added Chart

--- app/info.py
+++ app/info.py
@@ -2,6 +2,7 @@ from contextlib import contextmanager
 import requests
 import sqlite3
 import json
+import math
 import os
 import re
 
@@ -34,26 +35,27 @@ class YahooStock(object):
         raw_data = self.re_extract_json.search(raw_page).group(0)
         raw_data = raw_data.replace("root.App.main = ", "").replace("}(this)", "").replace(";", "")
         self.data = json.loads(raw_data)
+        self.quote_sum = self.data["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]
 
     @property
     def summary(self):
-        return self.data["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["summaryProfile"]
+        return self.quote_sum["summaryProfile"]
 
     @property
     def quote_data(self):
-        return self.data["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["quoteType"]
+        return self.quote_sum["quoteType"]
 
     @property
     def finanical_data(self):
-        return self.data["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["financialData"]
+        return self.quote_sum["financialData"]
 
     @property
     def details(self):
-        return self.data["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["summaryDetail"]
+        return self.quote_sum["summaryDetail"]
 
     @property
     def price(self):
-        return self.data["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["price"]
+        return self.quote_sum["price"]
 
 def get_tracked_stocks():
 
@@ -61,6 +63,13 @@ def get_tracked_stocks():
 
         cur.execute("SELECT DISTINCT stock FROM ticks ORDER BY stock ASC")
         return [val[0] for val in cur.fetchall()]
+
+def get_historical_closes(symbol, n=35):
+
+    with db() as (conn, cur):
+
+        cur.execute("SELECT adjclose FROM ticks WHERE stock=? ORDER BY date DESC LIMIT ?", [symbol, n])
+        return list(reversed([val[0] for val in cur.fetchall()]))
 
 def main():
 
@@ -73,6 +82,9 @@ def main():
         yahoo = YahooStock(stock)
         yahoo.download()
 
+        historical_data = get_historical_closes(stock)
+        chart_data = [close / historical_data[0] for close in historical_data]
+
         stock_data.append({
             "symbol": stock,
             "shortname": yahoo.quote_data["shortName"],
@@ -80,7 +92,10 @@ def main():
             "sector": yahoo.summary["sector"],
             "price": yahoo.price["regularMarketPrice"]["fmt"],
             "change": yahoo.price["regularMarketChange"]["fmt"],
-            "changepercent": yahoo.price["regularMarketChangePercent"]["fmt"]
+            "changepercent": yahoo.price["regularMarketChangePercent"]["fmt"],
+            "color": ("green" if yahoo.price["regularMarketChange"]["raw"] > 0 else "red"),
+            "barwidth": math.log(abs(yahoo.price["regularMarketChangePercent"]["raw"]) * 100 + 1) * 30,
+            "chartdata": ",".join(str(v) for v in chart_data)
         })
 
     data = {
